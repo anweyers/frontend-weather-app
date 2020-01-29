1261b781ea47f818683af976719006f2e80a7859
Anette <an.weyers@web.de>
2020-01-29 19:03:25 +0100

    feat: the complete application

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+
+function listening() {	
+  console.log("server running!");
+  console.log(`running on local host: ${port}`);
+}
+
+// GET route, callback function
+app.get('/all', sendData);
+function sendData(request, response) {
+  response.send(projectData);
+};
+
+app.post('/add', function (request, response) {
+  projectData = request.body;
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,89 @@
 /* Global Variables */
+// Setup empty JS object
+const data = {};
 
-// Create a new date instance dynamically with JS
+// Elements of the API Key
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = 'ee3675cc91d9e09ba9abc819e6ea6d97';
+const apiBridge = '&units=metric&APPID=';
+let zip = document.getElementById('zip');
+
+// HTML form ids
+ let feelings = document.getElementById('feelings');
+ let content = document.getElementById('content');
+
+// Date convert
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let theDate = d.getMonth()+1+'/'+ d.getDate()+'/'+ d.getFullYear();
+
+// Event handling
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+  const newZip =  document.getElementById('zip').value;
+  getWeather(baseURL, newZip, apiBridge, apiKey);
+  updateUI();
+}
+
+// Async GET request function, API call, returning an json-object
+const getWeather = async (baseURL, newZip, apiBridge, apiKey)=>{
+  const endpoint = baseURL + newZip + apiBridge + apiKey;
+  const res = await fetch(endpoint);
+  try {
+    const data = await res.json();
+    return data;
+  }  catch(error) {
+      console.log('error', error);
+  }
+}
+
+// Read temperature from API json file
+const getTemp = async () => {
+  const zip = document.getElementById('zip').value;
+  const endpoint = baseURL + zip + apiBridge + apiKey;
+  try {
+    const res = await fetch(endpoint);
+    if(res.ok) {
+      const jsonResponse = await res.json();
+      return jsonResponse.main.temp;
+    }
+  } catch(error) {
+    console.log('error', error);
+  }
+}
+
+//Async POST function to store data in the app
+const postData = async function ( url = '',data = {}) {
+  const res = await fetch (url, {
+      method:'POST',
+      credentials:'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  });
+  try {
+      const newData = res.json();
+      return newData;
+  }catch (error){
+      console.log('error', error);
+  };
+};
+
+/* Async function to update UI entry holder*/
+const updateUI = async () => {
+  const request = await fetch ('/all');
+  try{
+      data.date = theDate;
+      data.feelings = feelings.value;
+      data.temp = await getTemp();
+      date.innerHTML = 'Today is ' + data.date;
+      temp.innerHTML = 'The temperature is '+ data.temp + '&deg;C';
+      content.innerHTML = 'and I feel ' + data.feelings;
+    
+      document.getElementById('zip').value = "";
+      document.getElementById('feelings').value = "";
+  } catch (error){
+      console.log('error', error);
+  }
+}
